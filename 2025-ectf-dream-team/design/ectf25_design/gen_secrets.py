561cd5e2fca4541872db8d15756762c4315cb57d
krad2099 <153978998+krad2099@users.noreply.github.com>
2025-03-02 00:21:10 -0600

    Update gen_secrets.py

--- 2025-ectf-dream-team/design/ectf25_design/gen_secrets.py
+++ 2025-ectf-dream-team/design/ectf25_design/gen_secrets.py
@@ -1,3 +1,10 @@
+import argparse
+import json
+from pathlib import Path
+
+from loguru import logger
+
+
 def gen_secrets(channels: list[int]) -> bytes:
     """Generate the contents of the secrets file
 
@@ -15,3 +22,59 @@ def gen_secrets(channels: list[int]) -> bytes:
 
     # Return the secrets as a JSON-encoded byte string
     return json.dumps(secrets).encode()
+
+
+def parse_args():
+    """Define and parse the command line arguments
+
+    NOTE: Your design must not change this function
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--force",
+        "-f",
+        action="store_true",
+        help="Force creation of secrets file, overwriting existing file",
+    )
+    parser.add_argument(
+        "secrets_file",
+        type=Path,
+        help="Path to the secrets file to be created",
+    )
+    parser.add_argument(
+        "channels",
+        nargs="+",
+        type=int,
+        help="Supported channels. Channel 0 (broadcast) is always valid and will not"
+        " be provided in this list",
+    )
+    return parser.parse_args()
+
+
+def main():
+    """Main function of gen_secrets
+
+    You will likely not have to change this function
+    """
+    # Parse the command line arguments
+    args = parse_args()
+
+    secrets = gen_secrets(args.channels)
+
+    # Print the generated secrets for your own debugging
+    # Attackers will NOT have access to the output of this, but feel free to remove
+    #
+    # NOTE: Printing sensitive data is generally not good security practice
+    logger.debug(f"Generated secrets: {secrets}")
+
+    # Open the file, erroring if the file exists unless the --force arg is provided
+    with open(args.secrets_file, "wb" if args.force else "xb") as f:
+        # Dump the secrets to the file
+        f.write(secrets)
+
+    # For your own debugging. Feel free to remove
+    logger.success(f"Wrote secrets to {str(args.secrets_file.absolute())}")
+
+
+if __name__ == "__main__":
+    main()
